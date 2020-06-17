4e4546118e3349233f9387d28e18a206ce157bf4
Corey Coburn <corey@coburn.codes>
2020-06-16 22:54:58 -0700

    Declare strict types

--- app/Projects/Javascript/React.php
+++ app/Projects/Javascript/React.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Projects\Javascript;
 
 use App\Projects\Javascript\Builders\CreateReactApp;
--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Returns absolute base path of phar file.
  *
